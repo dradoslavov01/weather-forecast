13c2cc563e8fbafaf11378a88a2505001d6f778c
Yordan Radoslavov <yordan@example.com>
2026-01-11 21:50:42 +0200

    added aliases for imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "types": path.resolve(__dirname, "src/types"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
